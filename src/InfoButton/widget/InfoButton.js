4f8ae9bdf7cdb42da689027c543da66a953106b3
Conner Charlebois <cdcharlebois@gmail.com>
2019-04-16 12:21:40 -0400

    init

---
+++ src/InfoButton/widget/InfoButton.js
@@ -0,0 +1,84 @@
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+
+    "dojo/text!InfoButton/widget/template/InfoButton.html"
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, widgetTemplate) {
+    "use strict";
+
+    return declare("InfoButton.widget.InfoButton", [_WidgetBase, _TemplatedMixin], {
+
+        templateString: widgetTemplate,
+
+
+        widgetBase: null,
+
+        // Internal variables.
+        _handles: null,
+        _contextObj: null,
+
+        constructor: function () {
+            this._handles = [];
+        },
+
+        postCreate: function () {
+            logger.debug(this.id + ".postCreate");
+            this._setupButton();
+        },
+
+        update: function (obj, callback) {
+            logger.debug(this.id + ".update");
+
+            this._contextObj = obj;
+            this._updateRendering(callback);
+        },
+
+        resize: function (box) {
+            logger.debug(this.id + ".resize");
+        },
+
+        uninitialize: function () {
+            logger.debug(this.id + ".uninitialize");
+        },
+
+        _setupButton: function () {
+            this.connect(this.buttonNode, "click", this._handleClick.bind(this))
+        },
+
+        _handleClick: function () {
+            var messageString = "All these people are awesome: \n";
+            messageString += "Conner\n";
+            // TODO: Add your name here
+            mx.ui.info(messageString, false);
+        },
+
+        _updateRendering: function (callback) {
+            logger.debug(this.id + "._updateRendering");
+
+            this._executeCallback(callback, "_updateRendering");
+        },
+
+        // Shorthand for executing a callback, adds logging to your inspector
+        _executeCallback: function (cb, from) {
+            logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
+            if (cb && typeof cb === "function") {
+                cb();
+            }
+        }
+    });
+});
+
+require(["InfoButton/widget/InfoButton"]);
